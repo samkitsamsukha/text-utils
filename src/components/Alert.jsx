bba3ac698936c9ebc52ff88b3624759e8c2eac2f
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-04-09 01:21:47 +0530

    Updates to README.md and other minor changes

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -5,7 +5,6 @@ export default function Alert(props) {
         props.alert && <div>
         <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
                 {props.alert.msg}
-                
             </div>
         </div>
     )
